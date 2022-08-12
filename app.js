0f8447ad918755e541cf9a8c2e53accfa746c23c
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-12 11:21:34 -0700

    client side funcitonality for current poll & archive process WIP

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // import functions and grab DOM elements
+import { createPoll, getPolls } from "./fetch-utils.js";
+
 const submitPoll = document.getElementById('inputs');
+const currentTrackedPollEl = document.getElementById('poll-here');
+const voteOneUp = document.getElementById('votes-one-up');
+const voteTwoUp = document.getElementById('votes-two-up');
+const voteOneDown = document.getElementById('votes-one-down');
+const voteTwoDown = document.getElementById('votes-two-down');
 // console.log(submitPoll);
+const archivePoll = document.getElementById('finish-poll');
 // let state
 let poll = {
     question: '',
@@ -19,14 +27,65 @@ submitPoll.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(submitPoll);
     const question = formData.get('question');
-    console.log(question);
+    // console.log(question);
     const answerOne = formData.get('answer-one');
-    console.log(answerOne);
+    // console.log(answerOne);
     const answerTwo = formData.get('answer-two');
-    console.log(answerTwo);
-    
+    // console.log(answerTwo);
+
+    poll.question = question;
+    poll.answer1 = answerOne;
+    poll.answer2 = answerTwo;
+    poll.votes1 = 0;
+    poll.votes2 = 0;
+
+    // console.log(poll);
+    displayPoll();
+    submitPoll.reset();
 });
 
-function renderCurrentPollEl() {
+voteOneUp.addEventListener('click', () => {
+    poll.votes1++;
+    displayPoll();
+});
+voteOneDown.addEventListener('click', () => {
+    poll.votes1--;
+    displayPoll();
+});
+voteTwoUp.addEventListener('click', () => {
+    poll.votes2++;
+    displayPoll();
+});
+voteTwoDown.addEventListener('click', () => {
+    poll.votes2--;
+    displayPoll();
+});
 
+archivePoll.addEventListener('click', () => {
+    createPoll(poll);
+});
+
+function displayPoll() {
+    currentTrackedPollEl.textContent = '';
+    const pollEl = renderPoll(poll.question, poll.answer1, poll.answer2, poll.votes1, poll.votes2);
+    currentTrackedPollEl.append(pollEl);
+}
+
+function renderPoll(question, answerOne, answerTwo, votesOne, votesTwo) {
+    const div1 = document.createElement('div');
+    const questionEl = document.createElement('h2');
+    const answerOneEl = document.createElement('p');
+    const answerTwoEl = document. createElement('p');
+    const questOneVotes = document.createElement('p');
+    const questTwoVotes = document.createElement('p');
+
+    questionEl.textContent = question;
+    answerOneEl.textContent = answerOne;
+    answerTwoEl.textContent = answerTwo;
+    questOneVotes.textContent = votesOne;
+    questTwoVotes.textContent = votesTwo;
+
+    div1.append(questionEl, answerOneEl, questOneVotes, answerTwoEl, questTwoVotes);
+    
+    return div1;
 }

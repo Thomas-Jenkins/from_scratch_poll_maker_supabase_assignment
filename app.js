1e76fdf322f95a8155f3c9075b0a4b4fe0332470
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-11 17:33:42 -0700

    add html and css for application structure

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
-
+const 
 // let state
+let poll = {
+    question: '',
+    answer1: '',
+    answer2: '',
+    votes1: '',
+    votes2: ''
+};
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+function renderCurrentPollEl() {
+
+}

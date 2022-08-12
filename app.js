e470f3a5aa6b4d57779c72aa1ae8fbed35e0c573
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-12 09:33:38 -0700

    set up for form and log value to console as proof of concept.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
-const 
+const submitPoll = document.getElementById('inputs');
+// console.log(submitPoll);
 // let state
 let poll = {
     question: '',
@@ -13,6 +14,19 @@ let poll = {
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+submitPoll.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(submitPoll);
+    const question = formData.get('question');
+    console.log(question);
+    const answerOne = formData.get('answer-one');
+    console.log(answerOne);
+    const answerTwo = formData.get('answer-two');
+    console.log(answerTwo);
+    
+});
+
 function renderCurrentPollEl() {
 
 }
